1b8a2e8de1f44f6995eda432ccc0ce0c3527fa47
Julia Epanchintseva <47349264+mokingirdo@users.noreply.github.com>
2019-12-03 23:52:29 +0700

    frame created

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+package main.java;
+
+import main.java.view.MainFrame;
+
+public class Main extends MainFrame {
+
+    public static void main(String[] args) {
+        new Main();
+    }
+
+    Main(){
+        super();
+        setSize(600, 600);
+
+
+        setVisible(true);
+    }
+}
