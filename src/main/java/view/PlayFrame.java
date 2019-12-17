21bafdfe168456987059678e4b97971705987b7d
Julia Epanchintseva <47349264+mokingirdo@users.noreply.github.com>
2019-12-17 17:47:01 +0700

    class card created

--- src/main/java/view/PlayFrame.java
+++ src/main/java/view/PlayFrame.java
@@ -1,10 +1,7 @@
 package view;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.io.File;
-import java.io.IOException;
 
 public class PlayFrame extends MainFrame{
 
@@ -15,10 +12,8 @@ public class PlayFrame extends MainFrame{
         super();
         setSize(700, 600);
         setResizable(false);
-        //AddPanels();
+        AddPanels();
 
-        ImageComponent component = new ImageComponent();
-        add(component);
         setVisible(false);
     }
 
@@ -44,36 +39,4 @@ public class PlayFrame extends MainFrame{
     }
 }
 
-class ImageComponent extends JComponent
-{
-    public ImageComponent()
-    {
-        // Получаем изображения.
-        try
-        {
-            image = ImageIO.read(new File("resources/card_images/6d76bca8aa50fe6b50776d76c5259a22.jpg"));
 
-        }
-        catch(IOException e)
-        {
-            e.printStackTrace();
-        }
-    }
-    public void paintComponent(Graphics g)
-    {
-        if(image == null) return;
-        int imageWidth = image.getWidth(this);
-        int imageHeight = image.getHeight(this);
-
-        // Отображение рисунка в левом верхнем углу.
-        g.drawImage(image, 0, 0, null);
-
-        // Многократный вывод изображения в панели.
-
-        for(int i = 0; i * imageWidth <= getWidth(); i++)
-            for(int j = 0; j * imageHeight <= getHeight(); j++)
-                if(i + j > 0)
-                    g.copyArea(0, 0, imageWidth, imageHeight, i * imageWidth, j * imageHeight);
-    }
-    private Image image;
-}
