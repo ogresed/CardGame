bba7b484d5f5125537b5a3dbef2b134a7b149bdf
Sergey <ogresed@gmail.com>
2019-12-21 00:31:30 +0700

    constants access changed

--- src/main/java/game/field/FieldConstants.java
+++ src/main/java/game/field/FieldConstants.java
@@ -1,13 +1,13 @@
 package game.field;
 
 public class FieldConstants {
-    static final int MINIMAL_HORIZONTAL_SIZE = 4;
-    static final int MAXIMAL_HORIZONTAL_SIZE = 16;
-    static final int MINIMAL_VERTICAL_SIZE = 4;
-    static final int MAXIMAL_VERTICAL_SIZE = 12;
-    static final int MINIMAL_DIFFERENT_NUMBER = 4;
-    static final int MAXIMAL_DIFFERENT_NUMBER = 20;
-    static final int DELETED = -1;
+    public static final int MINIMAL_HORIZONTAL_SIZE = 4;
+    public static final int MAXIMAL_HORIZONTAL_SIZE = 16;
+    public static final int MINIMAL_VERTICAL_SIZE = 4;
+    public static final int MAXIMAL_VERTICAL_SIZE = 12;
+    public static final int MINIMAL_DIFFERENT_NUMBER = 4;
+    public static final int MAXIMAL_DIFFERENT_NUMBER = 20;
+    public static final int DELETED = -1;
 
     FieldConstants() {
 
