152eeecc9a8ac068c4aaac2d402f30da8588207d
Julia Epanchintseva <47349264+mokingirdo@users.noreply.github.com>
2019-12-21 00:44:22 +0700

    add authorization frame

---
+++ src/main/java/view/AuthorizationFrame.java
@@ -0,0 +1,12 @@
+package view;
+
+public class AuthorizationFrame extends MainFrame{
+
+    AuthorizationFrame(){
+
+        super();
+        setSize(700, 600);
+
+        setVisible(false);
+    }
+}
