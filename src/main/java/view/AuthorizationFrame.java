e3b7584801ff35c63a8e7d1d7e5d2488e3160974
Julia Epanchintseva <47349264+mokingirdo@users.noreply.github.com>
2019-12-21 02:30:03 +0700

    update authorization

--- src/main/java/view/AuthorizationFrame.java
+++ src/main/java/view/AuthorizationFrame.java
@@ -10,14 +10,19 @@ public class AuthorizationFrame extends MainFrame{
     private PlayFrame playFrame = new PlayFrame();
     private  JButton ok;
     private JPanel panel;
+    private JTextField textField;
+    private JLabel label = new JLabel("                Hello, please, enter your name");
+    private String name;
 
     AuthorizationFrame(){
         super();
-        setLayout(new BorderLayout(2,2));
-        setSize(700, 200);
+        setLayout(new BorderLayout(3,3));
+        setSize(400, 200);
         addButton();
 
-        add(panel, BorderLayout.SOUTH);
+        add(panel, BorderLayout.SOUTH);createTextArea();
+        add(textField, BorderLayout.CENTER);
+        add(label, BorderLayout.NORTH);
 
         setVisible(false);
     }
@@ -29,12 +34,23 @@ public class AuthorizationFrame extends MainFrame{
     private void addButton(){
         createPanel();
         ok = new JButton("ok");
-        ok.addActionListener(event -> playFrame.Play());
+        ok.addActionListener(event -> onPlay());
         panel.add(ok);
     }
 
     private void createPanel(){
-
         panel = new JPanel();
+    }
+
+    private void onPlay(){
+        if(textField.getText().isEmpty())
+            return;
+        name = textField.getText();
+        playFrame.Play();
+        setVisible(false);
+    }
+
+    private void createTextArea(){
+        textField = new JTextField();
     }
 }
